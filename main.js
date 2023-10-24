f87302a02235ad0d7b3524ed97fa4e0b905bad4c
Anais Laguna <annslaguna@gmail.com>
2023-10-23 18:31:03 -0600

    juego de bingo

---
+++ main.js
@@ -0,0 +1,18 @@
+let ballColor = ""
+let ballNum = 0
+
+function bingoGame (ballColor,ballNum){
+    let result = ""
+    if ((ballColor == "rojo"||"red") && (ballNum == 7)){
+        result = "Felicidades! Ganaste el primer premio!!"
+    } else if ((ballColor== "verde" || "green")&& (ballNum == 2)) {
+        result = "Felicidades! Eres acreedor al segundo premio!"
+    } else {
+        result = "Seguro tendrás más suerte la próxima vez!"
+    }
+    return result
+}
+
+console.log (bingoGame("azul",3));
+console.log (bingoGame("red",7));
+console.log(bingoGame("verde",2));
